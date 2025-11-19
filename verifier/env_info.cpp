16c10f44ed5128b7f147814871cb6713225455b9
jiaqiwang969 <jiaqiwang969@gmail.com>
2025-11-19 08:02:58 +0800

    verifier: query metal-cpp runtime only

--- verifier/env_info.cpp
+++ verifier/env_info.cpp
@@ -17,7 +17,9 @@
 #ifdef SCALABLE_CCD_WITH_CUDA
 #include <cuda_runtime.h>
 #endif
-#ifdef SCALABLE_CCD_WITH_METAL
+#if defined(SCALABLE_CCD_WITH_METALCPP)
+#include <scalable_ccd/metalcpp/runtime/runtime.hpp>
+#elif defined(SCALABLE_CCD_WITH_METAL)
 #include <scalable_ccd/metal/runtime/runtime.hpp>
 #endif
 #ifdef SCALABLE_CCD_WITH_METAL2
@@ -129,7 +131,9 @@ nlohmann::json collect_env_info()
 #else
     j["build"]["with_cuda"] = false;
 #endif
-#ifdef SCALABLE_CCD_WITH_METAL
+#if defined(SCALABLE_CCD_WITH_METALCPP)
+    j["build"]["with_metal"] = true;
+#elif defined(SCALABLE_CCD_WITH_METAL)
     j["build"]["with_metal"] = true;
 #else
     j["build"]["with_metal"] = false;
@@ -168,7 +172,18 @@ nlohmann::json collect_env_info()
 #endif
 
     // Metal info (best-effort; only on Apple + when compiled with Metal)
-#ifdef SCALABLE_CCD_WITH_METAL
+#if defined(SCALABLE_CCD_WITH_METALCPP)
+    try {
+        const auto& rt = scalable_ccd::metalcpp::MetalCppRuntime::instance();
+        bool ok = rt.available();
+        j["metal"]["available"] = ok;
+        if (ok) {
+            j["metal"]["device_name"] = "metal-cpp";
+        }
+    } catch (...) {
+        // ignore
+    }
+#elif defined(SCALABLE_CCD_WITH_METAL)
     try {
         const auto& rt = scalable_ccd::metal::MetalRuntime::instance();
         bool ok = rt.available();
